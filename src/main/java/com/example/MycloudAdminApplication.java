e3f6e5f2430756001300f5c93e163145f4502884
chenxingxing6 <1527072012@qq.com>
2019-04-21 21:46:43 +0800

    修复window问题

--- src/main/java/com/example/MycloudAdminApplication.java
+++ src/main/java/com/example/MycloudAdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan(basePackages = {"com.example.modules.*.dao"})
 @EnableSwagger2
 @EnableAsync
-@EnableEurekaClient
+//@EnableEurekaClient
 public class MycloudAdminApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
