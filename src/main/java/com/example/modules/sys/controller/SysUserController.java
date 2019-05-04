480194ea2b96d9a5e55c093a5c9e502c897f49c2
chenxingxing6 <1527072012@qq.com>
2019-05-04 23:04:45 +0800

    修复修改密码问题

--- src/main/java/com/example/modules/sys/controller/SysUserController.java
+++ src/main/java/com/example/modules/sys/controller/SysUserController.java
@@ -33,110 +33,114 @@ import java.util.Map;
 @RestController
 @RequestMapping("/sys/user")
 public class SysUserController extends AbstractController {
-	@Autowired
-	private ISysUserService sysUserService;
-	@Autowired
-	private ISysUserRoleService sysUserRoleService;
+    @Autowired
+    private ISysUserService sysUserService;
+    @Autowired
+    private ISysUserRoleService sysUserRoleService;
 
-	/**
-	 * 所有用户列表
-	 */
-	@RequestMapping("/list")
-	@RequiresPermissions("sys:user:list")
-	public R list(@RequestParam Map<String, Object> params){
-		params.put("type", UserEnum.BACK.getType());
-		PageUtils page = sysUserService.queryPage(params);
-		return R.ok().put("page", page);
-	}
+    /**
+     * 所有用户列表
+     */
+    @RequestMapping("/list")
+    @RequiresPermissions("sys:user:list")
+    public R list(@RequestParam Map<String, Object> params) {
+        params.put("type", UserEnum.BACK.getType());
+        PageUtils page = sysUserService.queryPage(params);
+        return R.ok().put("page", page);
+    }
 
-	/**
-	 * 获取登录的用户信息
-	 */
-	@RequestMapping("/info")
-	public R info(){
-		return R.ok().put("user", getUser());
-	}
+    /**
+     * 获取登录的用户信息
+     */
+    @RequestMapping("/info")
+    public R info() {
+        return R.ok().put("user", getUser());
+    }
 
-	/**
-	 * 修改登录用户密码
-	 */
-	@SysLog("修改密码")
-	@RequestMapping("/password")
-	public R password(String password, String newPassword){
-		Assert.isBlank(newPassword, "新密码不为能空");
+    /**
+     * 修改登录用户密码
+     */
+    @SysLog("修改密码")
+    @RequestMapping("/password")
+    public R password(String password, String newPassword) {
+        Assert.isBlank(newPassword, "新密码不为能空");
 
-		//原密码
-		password = ShiroUtils.sha256(password, getUser().getSalt());
-		//新密码
-		newPassword = ShiroUtils.sha256(newPassword, getUser().getSalt());
+        SysUserEntity userEntity = sysUserService.selectById(getUserId());
+        //原密码
+        String mdPassword = ShiroUtils.sha256(password, userEntity.getSalt());
+        //新密码
+        if (!mdPassword.equals(userEntity.getPassword())) {
+            return R.error("原密码不正确");
+        }
+        userEntity.setPassword(newPassword);
+        this.update(userEntity);
 
-		//更新密码
+        /*//更新密码
 		boolean flag = sysUserService.updatePassword(getUserId(), password, newPassword);
 		if(!flag){
 			return R.error("原密码不正确");
-		}
+		}*/
+        return R.ok();
+    }
 
-		return R.ok();
-	}
+    /**
+     * 用户信息
+     */
+    @RequestMapping("/info/{userId}")
+    @RequiresPermissions("sys:user:info")
+    public R info(@PathVariable("userId") Long userId) {
+        SysUserEntity user = sysUserService.selectById(userId);
 
-	/**
-	 * 用户信息
-	 */
-	@RequestMapping("/info/{userId}")
-	@RequiresPermissions("sys:user:info")
-	public R info(@PathVariable("userId") Long userId){
-		SysUserEntity user = sysUserService.selectById(userId);
+        //获取用户所属的角色列表
+        List<Long> roleIdList = sysUserRoleService.queryRoleIdList(userId);
+        user.setRoleIdList(roleIdList);
 
-		//获取用户所属的角色列表
-		List<Long> roleIdList = sysUserRoleService.queryRoleIdList(userId);
-		user.setRoleIdList(roleIdList);
+        return R.ok().put("user", user);
+    }
 
-		return R.ok().put("user", user);
-	}
+    /**
+     * 保存用户
+     */
+    @SysLog("保存用户")
+    @RequestMapping("/save")
+    @RequiresPermissions("sys:user:save")
+    public R save(@RequestBody SysUserEntity user) {
+        ValidatorUtils.validateEntity(user, AddGroup.class);
+        user.setType(Integer.valueOf(UserEnum.BACK.getType()));
+        sysUserService.save(user);
 
-	/**
-	 * 保存用户
-	 */
-	@SysLog("保存用户")
-	@RequestMapping("/save")
-	@RequiresPermissions("sys:user:save")
-	public R save(@RequestBody SysUserEntity user){
-		ValidatorUtils.validateEntity(user, AddGroup.class);
-		user.setType(Integer.valueOf(UserEnum.BACK.getType()));
-		sysUserService.save(user);
+        return R.ok();
+    }
 
-		return R.ok();
-	}
+    /**
+     * 修改用户
+     */
+    @SysLog("修改用户")
+    @RequestMapping("/update")
+    @RequiresPermissions("sys:user:update")
+    public R update(@RequestBody SysUserEntity user) {
+        ValidatorUtils.validateEntity(user, UpdateGroup.class);
+        user.setType(Integer.valueOf(UserEnum.BACK.getType()));
+        sysUserService.update(user);
 
-	/**
-	 * 修改用户
-	 */
-	@SysLog("修改用户")
-	@RequestMapping("/update")
-	@RequiresPermissions("sys:user:update")
-	public R update(@RequestBody SysUserEntity user){
-		ValidatorUtils.validateEntity(user, UpdateGroup.class);
-		user.setType(Integer.valueOf(UserEnum.BACK.getType()));
-		sysUserService.update(user);
+        return R.ok();
+    }
 
-		return R.ok();
-	}
+    /**
+     * 删除用户
+     */
+    @SysLog("删除用户")
+    @RequestMapping("/delete")
+    @RequiresPermissions("sys:user:delete")
+    public R delete(@RequestBody Long[] userIds) {
+        if (ArrayUtils.contains(userIds, 1L)) {
+            return R.error("系统管理员不能删除");
+        }
 
-	/**
-	 * 删除用户
-	 */
-	@SysLog("删除用户")
-	@RequestMapping("/delete")
-	@RequiresPermissions("sys:user:delete")
-	public R delete(@RequestBody Long[] userIds){
-		if(ArrayUtils.contains(userIds, 1L)){
-			return R.error("系统管理员不能删除");
-		}
-
-		if(ArrayUtils.contains(userIds, getUserId())){
-			return R.error("当前用户不能删除");
-		}
-		sysUserService.deleteBatchIds(Arrays.asList(userIds));
-		return R.ok();
-	}
+        if (ArrayUtils.contains(userIds, getUserId())) {
+            return R.error("当前用户不能删除");
+        }
+        sysUserService.deleteBatchIds(Arrays.asList(userIds));
+        return R.ok();
+    }
 }
