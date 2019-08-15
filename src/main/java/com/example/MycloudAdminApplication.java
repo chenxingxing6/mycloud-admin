a08938db0e0c079278ee5203f7de1332d4101caa
lanxinghua <lanxinghua@2dfire.com>
2019-08-15 10:36:40 +0800

    多环境配置

--- src/main/java/com/example/MycloudAdminApplication.java
+++ src/main/java/com/example/MycloudAdminApplication.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,9 +12,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan(basePackages = {"com.example.modules.*.dao"})
 @EnableSwagger2
 public class MycloudAdminApplication extends SpringBootServletInitializer {
+	@Value("${mycloud.env}")
+	private static String env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycloudAdminApplication.class, args);
+		System.out.println("启动成功。。。" + env);
 	}
 
 	@Override
--- src/test/java/com/example/BaseTest.java
+++ src/test/java/com/example/BaseTest.java
@@ -3,6 +3,7 @@ package com.example;
 import com.example.common.utils.IdGen;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,9 +15,13 @@ public class BaseTest {
 	@Resource
 	IdGen idGen;
 
+	@Value("${mycloud.env}")
+	private String env;
+
 	@Test
 	public void contextLoads() {
 		System.out.println(idGen.nextId());
+		System.out.println(env);
 	}
 }
 
