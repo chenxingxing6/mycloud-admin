2105cc55d81bc1f6acebb34bdc45d22007d336de
lanxinghua <lanxinghua@2dfire.com>
2019-08-16 15:51:53 +0800

    easyMock

--- src/test/java/com/example/mock/EasyMockTest.java
+++ src/test/java/com/example/mock/EasyMockTest.java
@@ -1,11 +1,20 @@
 package com.example.mock;
 
+import com.alibaba.common.convert.Convert;
+import com.alibaba.fastjson.JSON;
+import com.example.modules.sys.dao.SysConfigDao;
+import com.example.modules.sys.dao.SysLogDao;
+import com.example.modules.sys.entity.SysConfigEntity;
+import com.example.modules.sys.entity.SysLogEntity;
 import com.example.modules.sys.service.ISysConfigService;
 import com.example.modules.sys.service.ISysLogService;
+import com.example.modules.sys.service.impl.SysConfigServiceImpl;
 import com.example.modules.sys.service.impl.SysLogServiceImpl;
-import org.easymock.EasyMockRunner;
-import org.easymock.Mock;
-import org.easymock.TestSubject;
+import org.easymock.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockServletContext;
@@ -15,16 +24,21 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * User: lanxinghua
  * Date: 2019/8/16 10:53
- * Desc:
+ * Desc: 步骤
+ * 1.生成mock接口
+ * 2.设置预期行为
+ * 3.将mock对象切换到replay状态
+ * 4.测试
+ * 5.验证mock对象行为
  */
 @RunWith(EasyMockRunner.class)
-public class EasyMockTest {
+public class EasyMockTest extends EasyMockSupport {
 
     @TestSubject
-    private ISysLogService logService = new SysLogServiceImpl();
+    private ISysConfigService configService = new SysConfigServiceImpl();
 
     @Mock
-    private ISysConfigService configService;
+    private SysConfigDao configDao;
 
     private HttpServletRequest servletRequest = createMockHttpServeletRequest();
 
@@ -36,4 +50,41 @@ public class EasyMockTest {
         MockServletContext context = new MockServletContext();
         return new MockHttpServletRequest(context);
     }
+
+    private SysConfigEntity entity = null;
+    private static final String id = "10000";
+
+    @Before
+    public void init(){
+        entity = new SysConfigEntity();
+        entity.setId(Convert.asLong(id));
+        entity.setRemark("remark");
+        entity.setParamKey("key");
+        entity.setParamValue("value");
+    }
+
+    @Test
+    public void test01(){
+        // 1.生成mock接口,如果需要mock多个接口，用controllerMock来管理
+        // IMocksControl control = EasyMock.createControl();
+        // SysConfigDao configDao = control.createMock(SysConfigDao.class);
+        // SysLogDao logDao = control.createMock(SysLogDao.class);
+
+        // 2.设置预期行为
+        EasyMock.expect(configDao.selectById(id)).andReturn(entity);
+
+        // 3.将mock切换到replay状态
+        replayAll();
+
+        // 4.测试
+        SysConfigEntity result = configService.selectById(id);
+
+        // 5.验证mock对象行为
+        // EasyMock.verify(configDao);
+        verifyAll();
+
+        System.out.println(JSON.toJSONString(result));
+        Assert.assertNotNull(result);
+    }
+
 }
